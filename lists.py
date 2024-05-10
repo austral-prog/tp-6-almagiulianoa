23e859e3044ca63737689ee7e6ccb7748380ba15
almagiulianoa <162723354+almagiulianoa@users.noreply.github.com>
2024-05-10 13:01:35 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,20 +1,37 @@
-# Replace the "ANSWER HERE" with your answer
-
 def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    if list_to_remove_elements == []:
+        return list_to_remove_elements
+    del list_to_remove_elements[0]
+    
+    if len(list_to_remove_elements) > 3:
+        del list_to_remove_elements[3]
+    else:
+        return list_to_remove_elements
 
+    if len(list_to_remove_elements) > 3:
+        del list_to_remove_elements[3]
+        return list_to_remove_elements
+    else:
+        return list_to_remove_elements
 
 def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
-
+    return ["Pink"] + list_to_add_elements + ["Yellow"]
 
 def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
-
-
+    if list_to_check == []:
+        return True
+    else:
+        return False
 def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
-
-
+    if len(list_to_compare1) > 2 and len(list_to_compare2) > 2:
+        if (list_to_compare1[2] == list_to_compare2[2]):
+            return True
+    else:
+        return False
+        
 def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+    lista1 = list_of_lists_to_modify[0][0:2]
+    lista2 = list_of_lists_to_modify[1][1:4]
+    lista3 = list_of_lists_to_modify[2][-2:]
+
+    return [lista1, lista2, lista3]
